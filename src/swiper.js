2d470066320f5b041477d152e31865b9fb24a33a
Victoria Serzhanova <victoriaserzhanova@gmail.com>
2020-11-17 00:52:08 +0300

    adaptive 1200

--- src/swiper.js
+++ src/swiper.js
@@ -18,7 +18,7 @@ const mySwiper = new Swiper('.gallery__swiper-container',  {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-})
+});
 
 //swiper in first section
 const mySwiperFirst = new Swiper('.first__swiper-container',  {
@@ -34,4 +34,30 @@ const mySwiperFirst = new Swiper('.first__swiper-container',  {
 		nextEl: '.first__slider-btn--next',
 		prevEl: '.first__slider-btn--prev',
 	},
-})
+});
+
+// play video button
+const playButtonFirst = document.querySelectorAll('.main-slider__play');
+
+playButtonFirst.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+		video.play();
+		e.currentTarget.style.display = 'none';
+		setTimeout(() => {
+			video.volume = 0.5;
+		}, 1000);
+	});
+});
+
+
+mySwiperFirst.on('transitionEnd', function () {
+	let videos = document.querySelectorAll('.first__slider video');
+	videos.forEach((el) => {
+		el.pause();
+		el.currentTime = 0;
+	});
+	playButtonFirst.forEach((el) => {
+		el.style.display = 'block';
+	});
+});
